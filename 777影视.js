144301ac6e82d6a6d36d7cff9e429559ed98384d
xiaoshuangdashu <159227073+xiaoshuangdashu@users.noreply.github.com>
2025-07-06 19:42:00 +0800

    Update 777影视.js

--- 777影视.js
+++ 777影视.js
@@ -1,6 +1,6 @@
 var rule={
     title:'唐人街影视[飞]',
-    host:'https://www.chinatownfilm.com',
+    host:'https://www.5s9c5q3v8l8.shop',
     url:'/vodtype/fyclass/page/fypage.html',
     // url:'/vodtype/fyfilter-----------.html,
     filterable:1,//是否启用分类筛选,
