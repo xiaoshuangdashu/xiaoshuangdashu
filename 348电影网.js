a83300cce446182d6e9cdc63c89b06145bab4ed3
xiaoshuangdashu <159227073+xiaoshuangdashu@users.noreply.github.com>
2025-07-10 14:41:44 +0800

    Update 348电影网.js

--- 348电影网.js
+++ 348电影网.js
@@ -1,4 +1,4 @@
 [{
   "name": "2400多部恐怖电影[恐怖电影]",
-  "url": "http://host407776532.s1169.pppf.com.cn/app/lib/2400部恐怖电影.txt&&&http://r.photo.store.qq.com/psc?/V12LWP8V1gfI1T/QNsgOSLzUrTyB8UN2gSlSB5*eQLQvajGQsd3mteuCeHCJsEbFZt.KV0TEZX.GS.DPHXdE1QlC9jjHWnTrgFr4Q!!/r"
+  "url": "http://host407776532.s1169.pppf.com.cn/app/lib/2400部恐怖电影.txt&&&http://photocq.photo.store.qq.com/psc?/V12LWP8V1gfI1T/QNsgOSLzUrTyB8UN2gSlSF2fA7mVDQNNTsY2fXr7g8mcmx6*2RrbN.bSC*NMiYe.zYldCS8twTUKhbrGmMGjZw!!/b&bo=WAIgA1gCIAMCCS0!&rf=viewer_4"
     }]
